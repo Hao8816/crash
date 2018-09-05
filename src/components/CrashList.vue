27fcd3f222b68d9964885794b1f96230f88316f2
chenhao63 <chenhao63@meituan.com>
2018-09-05 20:37:40 +0800

    add file

---
+++ src/components/CrashList.vue
@@ -0,0 +1,32 @@
+<template>
+  <h1>{{crashList}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'crash-list',
+  data () {
+    return {
+      crashList: [{name: '测试1', time: '2018-01-01', type: 'bug'}]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
