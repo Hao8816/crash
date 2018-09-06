ad1b9c0a41be40f1fc79dfe76f860f10d9dce653
chenhao63 <chenhao63@meituan.com>
2018-09-06 11:24:41 +0800

    搭建页面

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,76 @@
 <template>
   <div id="app">
     <el-container>
-  <el-header>Header</el-header>
-  <el-container>
-    <el-aside width="200px">Aside</el-aside>
-    <el-main>Main</el-main>
-  </el-container>
-</el-container>
-    <el-container>
-      <el-menu
-        :default-active="activeIndex"
-        class="el-menu-demo"
-        mode="horizontal"
-        @select="handleSelect"
-        background-color="#545c64"
-        text-color="#fff"
-        active-text-color="#ffd04b">
-        <el-menu-item index="1">处理中心</el-menu-item>
-        <el-submenu index="2">
-          <template slot="title">我的工作台</template>
-          <el-menu-item index="2-1">选项1</el-menu-item>
-          <el-menu-item index="2-2">选项2</el-menu-item>
-          <el-menu-item index="2-3">选项3</el-menu-item>
-          <el-submenu index="2-4">
-            <template slot="title">选项4</template>
-            <el-menu-item index="2-4-1">选项1</el-menu-item>
-            <el-menu-item index="2-4-2">选项2</el-menu-item>
-            <el-menu-item index="2-4-3">选项3</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="3">消息中心</el-menu-item>
-      </el-menu>
       <el-header>
-
+        <el-row>
+          <el-col :span="12"><div class="grid-content bg-purple">Header</div></el-col>
+          <el-col :span="12">
+            <div class="grid-content nav-right">
+              <el-dropdown trigger="click">
+                <span class="el-dropdown-link">
+                  下拉菜单<i class="el-icon-arrow-down el-icon--right"></i>
+                </span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item>黄金糕</el-dropdown-item>
+                  <el-dropdown-item>狮子头</el-dropdown-item>
+                  <el-dropdown-item>螺蛳粉</el-dropdown-item>
+                  <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+                  <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </div>
+          </el-col>
+        </el-row>
       </el-header>
-      <el-main>
-        <CrashList msg="Welcome to Your Vue.js App"/>
-        <el-button>el-button</el-button>
-      </el-main>
+      <el-container>
+        <el-aside width="200px">
+          <el-row class="tac">
+            <el-col>
+              <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo"
+                @open="handleOpen"
+                @close="handleClose">
+                <el-submenu index="1">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>导航一</span>
+                  </template>
+                  <el-menu-item-group>
+                    <template slot="title">分组一</template>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="分组2">
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                  </el-menu-item-group>
+                  <el-submenu index="1-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-menu-item index="2">
+                  <i class="el-icon-menu"></i>
+                  <span slot="title">导航二</span>
+                </el-menu-item>
+                <el-menu-item index="3" disabled>
+                  <i class="el-icon-document"></i>
+                  <span slot="title">导航三</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <i class="el-icon-setting"></i>
+                  <span slot="title">导航四</span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-aside>
+        <el-main>
+          <keep-alive>
+            <router-view></router-view>
+          </keep-alive>
+        </el-main>
+      </el-container>
     </el-container>
   </div>
 </template>
@@ -56,7 +89,10 @@ export default {
     CrashList
   },
   methods: {
-    handleSelect (key, keyPath) {
+    handleOpen (key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose (key, keyPath) {
       console.log(key, keyPath)
     }
   }
@@ -68,7 +104,40 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+}
+.nav-right{
+  text-align: right;
+}
+.el-header, .el-footer {
+  background-color: #333;
+  color: #fff;
+  line-height: 60px;
+}
+.el-aside {
+  color: #333;
+  line-height: 200px;
+}
+.el-main {
+  background-color: #E9EEF3;
+  color: #333;
+  min-height: 700px;
+}
+.el-dropdown{
+  color: #fff;
+  cursor: pointer;
+}
+
+body > .el-container {
+  margin-bottom: 40px;
+}
+
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
 }
 </style>
--- src/components/CrashList.vue
+++ src/components/CrashList.vue
@@ -1,5 +1,31 @@
 <template>
-  <h1>{{crashList}}</h1>
+  <div class="crash-wrapper">
+    <div class="crash-fliter">
+      <el-form :inline="true" :model="formInline" class="demo-form-inline">
+        <el-form-item label="审批人">
+          <el-input v-model="formInline.user" placeholder="审批人"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+          <el-select v-model="formInline.region" placeholder="活动区域">
+            <el-option label="区域一" value="shanghai"></el-option>
+            <el-option label="区域二" value="beijing"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="onSubmit">查询</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+    <div class="crash-item" v-for="crash in crashList" :key="crash.name">
+      <div slot="header" class="clearfix">
+        <span>{{crash.name}}</span>
+        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+      </div>
+      <div class="text item">
+        {{'列表内容 ' + crash.time}}
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,7 +33,17 @@ export default {
   name: 'crash-list',
   data () {
     return {
-      crashList: [{name: '测试1', time: '2018-01-01', type: 'bug'}]
+      crashList: [
+        {name: '测试1', time: '2018-01-01', type: 'bug'},
+        {name: '测试2', time: '2018-01-02', type: 'bug'},
+        {name: '测试3', time: '2018-01-04', type: 'bug'}
+      ],
+      formInline: {}
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('xxx')
     }
   }
 }
@@ -22,11 +58,28 @@ ul {
   list-style-type: none;
   padding: 0;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
 a {
   color: #42b983;
+}
+.text {
+    font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+.clearfix:after {
+  clear: both
+}
+.crash-item{
+  padding: 15px 20px;
+  background-color: #fff;
+  border-bottom: 1px solid rgba(0,0,0,0.125)
 }
 </style>
