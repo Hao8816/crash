633b9b1919ce5be7fe54e5fe4166920158a18757
chenhao63 <chenhao63@meituan.com>
2018-09-06 10:10:04 +0800

    添加组件配置

--- src/App.vue
+++ src/App.vue
@@ -8,30 +8,31 @@
   </el-container>
 </el-container>
     <el-container>
-      <el-header>
-        <el-menu
-          :default-active="activeIndex"
-          class="el-menu-demo"
-          mode="horizontal"
-          @select="handleSelect"
-          background-color="#545c64"
-          text-color="#fff"
-          active-text-color="#ffd04b">
-          <el-menu-item index="1">处理中心</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+        <el-menu-item index="1">处理中心</el-menu-item>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-submenu index="2-4">
+            <template slot="title">选项4</template>
+            <el-menu-item index="2-4-1">选项1</el-menu-item>
+            <el-menu-item index="2-4-2">选项2</el-menu-item>
+            <el-menu-item index="2-4-3">选项3</el-menu-item>
           </el-submenu>
-          <el-menu-item index="3">消息中心</el-menu-item>
-        </el-menu>
+        </el-submenu>
+        <el-menu-item index="3">消息中心</el-menu-item>
+      </el-menu>
+      <el-header>
+
       </el-header>
       <el-main>
         <CrashList msg="Welcome to Your Vue.js App"/>
--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
 import './style/main.css'
+import './plugins/element.js'
 
-Vue.use(Element)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
